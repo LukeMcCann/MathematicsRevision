46fd8aa35ba18cd8c62de41235e947e86f730cc1
Luke McCann <luke-mccann@victorytechnology.co.uk>
2023-06-10 21:17:07 +0100

    Update cheatsheet.m

--- Matlab/cheatsheet.m
+++ Matlab/cheatsheet.m
@@ -1,10 +1,15 @@
 % Matlab Syntax
 
-% MatLab stands for
+% MatLab stands for 
 % Matrices Laboratory.
 
 % As such, everything in matlab
 % is stored in matrices
+
+% semicolons are different to
+% what you may be used to. 
+% In MatLab they are called suppression
+% they are used to suppress output.
 
 % We can create a variable as such:
 a = 5;
@@ -13,7 +18,7 @@ a = 5;
 % is no output
 b = 3;
 
-% If we omit the semicolon
+% If we omit the semicolon 
 % the evaluation will be output
 % to the console
 a + b
@@ -22,8 +27,11 @@ a + b
 % We can define strings
 hello = "hello";
 
+% We can also define chars
+t = 't';
+
 % We can define strings
-% with multiple columns and
+% with multiple columns and 
 % Rows in our matrices
 testMatrix = ["Hello", "World" ; "Goodbye", "World"];
 
@@ -43,3 +51,32 @@ two = logical([0 1 0]);
 % We can check defined variables
 % In the environment using
 whos
+
+
+% We have access to other functions
+% and operations
+sqrt(5);
+
+% We can clear the console with
+% clc;
+
+% And clear Variables with
+% clearvars
+
+% Commas can be used to write 
+% multiple commands on one line
+you = "Malic", me = "Luke"
+
+
+% And of course we have mathematical operators
+a = 10;
+b = 2;
+
+a * b
+a / b
+a + b
+a - b
+a^b
+
+% Matlab supports compound expressions
+sqrt(a * b)
